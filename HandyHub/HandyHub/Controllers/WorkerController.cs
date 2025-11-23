6bb55fe11c4007c9587c31e17c6aea82342ea151
abdo0101010 <abdoaaasss123456@gmail.com>
2025-11-24 01:44:56 +0200

    Add WorkerController and Search Views

---
+++ HandyHub/HandyHub/Controllers/WorkerController.cs
@@ -0,0 +1,21 @@
+﻿using HandyHub.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HandyHub.Controllers
+{
+    public class WorkerController : Controller
+    {
+        IWorkerService workerService;
+        public WorkerController(IWorkerService workerService)
+        {
+            this.workerService = workerService;
+        }
+        [HttpGet]
+        public IActionResult Search()
+        {
+            var workers = workerService.GetAllWorkersWithPortfolioWithUserWithReviews();
+
+            return View(workers);
+        }
+    }
+}
